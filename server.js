9242ac927d021400e3cc193d06a6cbbd4a2a91bc
bear-evans <bear-evans@outlook.com>
2021-07-24 16:41:31 -0400

    Added a header and reorganized code

--- server.js
+++ server.js
@@ -2,28 +2,33 @@
 // Imports and Globals
 // ===========================
 require("dotenv").config();
+const cTable = require("console.table");
 const express = require("express");
 const app = express();
+const chalk = require("chalk");
 const mysql = require("mysql2");
-const cTable = require("console.table");
 const grizzlyDB = require("./lib/grizzlyEmployee.js");
 const prompts = require("./lib/prompts.js");
 const inquirer = require("inquirer");
 
 // Variables needed by MySQL are stored here. They are either retrieved
 // from the environment variables or set to generic defaults
+const PORT = process.env.PORT || 3306;
+const HOSTNAME = process.env.PORT || "localhost";
+const USERNAME = process.env.USER || "root";
+const PW = process.env.PW || "password";
 
 app.use(express.urlencoded({ extended: false }));
 
 // Creates a new GrizzlyDB object that represents the MySQL connection
 // and contains all necessary query functions
-const grizzly = new grizzlyDB();
+const grizzly = new grizzlyDB(HOSTNAME, USERNAME, PW, PORT);
 
 // ===========================
 // Inquirer Prompt Functions
 // ===========================
-function main() {
-  inquirer.prompt(prompts).then((answer) => {
+function mainPrompt() {
+  inquirer.prompt(prompts.main).then((answer) => {
     switch (answer.main) {
       case "View and Compare Employees, Roles, or Departments":
         viewPrompt();
@@ -38,8 +43,50 @@ function main() {
   });
 }
 
+function viewPrompt() {
+  inquirer.prompt(prompts.view).then((answer) => {
+    switch (answer.view) {
+      case "View Employees":
+        grizzly.viewEmployee();
+        viewPrompt();
+        break;
+      case "View Roles":
+        grizzly.viewRole();
+        viewPrompt();
+        break;
+      case "View Departments":
+        grizzly.viewDepartment();
+        viewPrompt();
+        break;
+      case "Go Back":
+        mainPrompt();
+        break;
+    }
+  });
+}
+
 function init() {
-  main();
+  // It prints a really cool header, I promise.
+  const header = `${chalk.red(
+    "========================================================"
+  )}
+ ${chalk.yellow("__         __")}
+${chalk.yellow('/  \\.-"""-./  \\')}
+${chalk.yellow("\\    -   -    /")}                  ${chalk.yellowBright(
+    "Grizzly"
+  )}
+ ${chalk.yellow("|")}   ${chalk.green("o   o")}   ${chalk.yellow(
+    "|"
+  )}                   ${chalk.yellowBright("Employee")}
+ ${chalk.yellow("\\")}  .-'''-.  ${chalk.yellow(
+    "/"
+  )}                  ${chalk.yellowBright("Management")}
+  ${chalk.yellow("'-")}\\__${chalk.gray("Y")}__/${chalk.yellow("-'")}
+     '---'
+${chalk.red("========================================================")}`;
+
+  console.log(header);
+  mainPrompt();
 }
 
 // ===========================
