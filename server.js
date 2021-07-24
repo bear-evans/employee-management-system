b2f2918fa89f4bd51f8a583287ca18133100a362
bear-evans <bear-evans@outlook.com>
2021-07-24 13:23:06 -0400

    GrizzlyDB object now connects to database

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mysql = require("mysql2");
 const cTable = require("console.table");
 const grizzlyDB = require("./lib/grizzlyEmployee.js");
 const prompts = require("./lib/prompts.js");
@@ -11,20 +12,37 @@ const inquirer = require("inquirer");
 
 // Variables needed by MySQL are stored here. They are either retrieved
 // from the environment variables or set to generic defaults
-const PORT = process.env.PORT || 3001;
-const HOSTNAME = process.env.HOSTNAME || "localhost";
-const USERNAME = process.env.USERNAME || "root";
-const PW = process.env.PW || "root";
 
 app.use(express.urlencoded({ extended: false }));
 
 // Creates a new GrizzlyDB object that represents the MySQL connection
 // and contains all necessary query functions
-const grizzly = new grizzlyDB(HOSTNAME, PORT, USERNAME, PW);
+const grizzly = new grizzlyDB();
 
 // ===========================
 // Inquirer Prompt Functions
 // ===========================
 function main() {
-  inquirer.prompt().then();
+  inquirer.prompt(prompts).then((answer) => {
+    switch (answer.main) {
+      case "View and Compare Employees, Roles, or Departments":
+        viewPrompt();
+        break;
+      case "Add or Update Employees, Roles, or Departments":
+        addPrompt();
+        break;
+      case "Delete Employees, Roles, or Departments":
+        deletePrompt();
+        break;
+    }
+  });
 }
+
+function init() {
+  main();
+}
+
+// ===========================
+// Goooooo!
+// ===========================
+init();
