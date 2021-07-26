96f1c5c9d69cb0d69d3d619cf91914354b2c714c
bear-evans <bear-evans@outlook.com>
2021-07-26 15:32:11 -0400

    added missing update manager prompt

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,7 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Bear", "Evans", 3, 1), ("Alan", "Turing", 1, 1), ("Morgan", "Coleman", 2, 1);
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Customer Support Technician", 30000, 4), ("Warehouse Worker", 18000, 2), ("Consultant", 40000, 1), ("Researcher", 40000, 1);
+VALUES ("Customer Support Technician", 30000, 4), ("Warehouse Worker", 18000, 3), ("Consultant", 40000, 1), ("Researcher", 40000, 2);
 
 INSERT INTO department (name)
 VALUES ("Executive"), ("Research"), ("Shipping"), ("Customer Support");
